748d505c7121539b7a1b3dfe2f52c7fbe1ac56d8
Nikita <Zungarcheg@gmail.com>
2020-09-05 22:10:11 +0300

    Updates

---
+++ precache-manifest.ed1bc8f63c0729218f10f83a07a6a016.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9219bdefb1b168d0f59cc3feebfb198b",
+    "url": "/goit-react-hw-02-feedback/index.html"
+  },
+  {
+    "revision": "964bb3e9a47f3ac1d740",
+    "url": "/goit-react-hw-02-feedback/static/css/main.92725b91.chunk.css"
+  },
+  {
+    "revision": "2e23161ad69179c19bb8",
+    "url": "/goit-react-hw-02-feedback/static/js/2.0b076d13.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-02-feedback/static/js/2.0b076d13.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "964bb3e9a47f3ac1d740",
+    "url": "/goit-react-hw-02-feedback/static/js/main.e6e37cde.chunk.js"
+  },
+  {
+    "revision": "b58a435a92ac135c88f5",
+    "url": "/goit-react-hw-02-feedback/static/js/runtime-main.82cfee65.js"
+  },
+  {
+    "revision": "58aef543c97bbaf6a9896e8484456d98",
+    "url": "/goit-react-hw-02-feedback/static/media/Roboto-Medium.58aef543.ttf"
+  },
+  {
+    "revision": "11eabca2251325cfc5589c9c6fb57b46",
+    "url": "/goit-react-hw-02-feedback/static/media/Roboto-Regular.11eabca2.ttf"
+  }
+]);
